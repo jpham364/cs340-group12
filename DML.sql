56ba63730a0034c758fc34ef68535ca3861360cc
jpham364 <phamjonathan364@outlook.com>
2023-05-11 13:25:44 -0700

    Added more DML operations

--- DML.sql
+++ DML.sql
@@ -15,130 +15,138 @@ SELECT * FROM ProductType;
 SELECT * FROM OrderEquipment;
 
 
--- Select Orders from Each Person
+-- Select a user data
+SELECT userID, firstName, lastName, address, phoneNumber, email
+	FROM Users WHERE userId = :userID;
+
+-- Select all orders from user
+SELECT firstName, lastName, Users.userID, OrderID from Users INNER JOIN Orders ON Users.userID = Orders.userID;
 
 
+-- INSERT STATEMENTS --
+-- Insert new User
+INSERT INTO Users(
+	firstName, 
+	lastName, 
+	address, 
+	phoneNumber,
+	email
+) 
+VALUES 
+(
+	:first_name_input, 
+	:last_name_input, 
+	:address_input, 
+	:phonenumber_input, 
+	:email_input
+);
 
 
-
--- -- INSERT STATEMENTS --
--- -- Insert new User
--- INSERT INTO Users(
--- 	firstName, 
--- 	lastName, 
--- 	address, 
--- 	phoneNumber,
--- 	email
--- ) 
--- VALUES 
--- (
--- 	:first_name_input, 
--- 	:last_name_input, 
--- 	:address_input, 
--- 	:phonenumber_input, 
--- 	:email_input
--- );
+-- Insert Order
+INSERT INTO Orders(
+	userID,
+	orderDate, 
+	numItems, 
+	totalCost
+) 
+VALUES 
+(
+	:userID_dropdown, 
+	:order_date_input, 
+	0, 
+	0
+);
 
 
--- -- Insert Order
--- INSERT INTO Orders(
--- 	userID,
--- 	orderDate, 
--- 	numItems, 
--- 	totalCost
--- ) 
--- VALUES 
--- (
--- 	:userID_dropdown, 
--- 	:order_date_input, 
--- 	0, 
--- 	0
--- );
+-- Insert Equipment
+INSERT INTO Equipment
+(
+	equipmentName, 
+	equipmentDescription, 
+	equipmentCost, 
+	equipmentStock, 
+	productTypeID
+)
+VALUES
+(
+
+	:equipment_name_input, 
+	:equipment_description_input, 
+	:equipment_cost_input, 
+	:equipment_stock_input, 
+	:productTypeID_dropdown
+
+);
+
+-- Insert Rewiew
+INSERT INTO Reviews (
+    userID,
+    equipmentID,
+    reviewDescription,
+    stars
+)
+VALUES
+(
+    :userIDDropdown,
+    :equipmentIDDropdown,
+    :reviewDescriptionInput,
+    :starInput
+)
+
+-- Insert Product Type
+INSERT INTO ProductType (
+    productTypeID,
+    typeDescription
+)
+VALUES
+(
+    :productTypeIDInput,
+    :typeDescriptionInput
+);
+
+--Insert orderEquipment (M:N relationship addition)
+INSERT INTO OrderEquipment(
+
+    orderID,
+    equipmentID,
+
+    amount,
+    cost
+
+)
+VALUES
+(
+    :orderIDDropdown,
+    :equipmentIDDropdown,
+    :amountInput,
+    :equipmentcostTimesAmount
+);
 
 
--- -- Insert Equipment
--- INSERT INTO Equipment
--- (
--- 	equipmentName, 
--- 	equipmentDescription, 
--- 	equipmentCost, 
--- 	equipmentStock, 
--- 	productTypeID
--- )
--- VALUES
--- (
+-- delete a user
+DELETE FROM Users WHERE userID = :UserID_from_user_table;
 
--- 	:equipment_name_input, 
--- 	:equipment_description_input, 
--- 	:equipment_cost_input, 
--- 	:equipment_stock_input, 
--- 	:productTypeID_dropdown
+-- delete an equipment
+DELETE FROM Equipment where equipmentID = :equipmentID_from_equipment_table;
 
--- );
+-- delete review (removes 1:M relationship)
+DELETE FROM Reviews where reviewID = :reviewID_from_review_table;
 
--- -- Insert Rewiew
--- INSERT INTO Reviews (
---     userID,
---     equipmentID,
---     reviewDescription,
---     stars
--- )
--- VALUES
--- (
---     :userIDDropdown,
---     :equipmentIDDropdown,
---     :reviewDescriptionInput,
---     :starInput
--- )
+-- dis-associate an equipment from an order (M:N relationship deletion)
+DELETE FROM equipmentOrders where equipmentID = :equipmentID_from_equipment_table AND userID = UserID_from_user_table;
 
--- -- Insert Product Type
--- INSERT INTO ProductType (
---     productTypeID,
---     typeDescription
--- )
--- VALUES
--- (
---     :productTypeIDInput,
---     :typeDescriptionInput
--- );
+-- Remove Equipment Stock
+UPDATE Equipment
+SET equipmentStock = ((select equipmentStock from Equipment where :equipment_id_from_equipment_table) - :chosen_amount_removed)
+WHERE equipmentID = :equipmentID_and_name
 
--- --Insert orderEquipment
--- INSERT INTO OrderEquipment(
-
---     orderID,
---     equipmentID,
-
---     amount,
---     cost
-
--- )
--- VALUES
--- (
---     :orderIDDropdown,
---     :equipmentIDDropdown,
---     :amountInput,
---     :equipmentcostTimesAmount
--- );
+-- Update Total cost of user's purchase
+UPDATE Orders
+SET numItems = (select sum(amount) from OrderEquipment where orderID 
+                IN (SELECT orderID from Orders where :user_ID_from_table))
+WHERE orderID = (SELECT orderID from Orders where (:user_ID AND :order_Date));
 
 
--- -- delete a user
--- DELETE FROM Users WHERE userID = :UserID_from_user_table;
-
--- -- delete an equipment
--- DELETE FROM Equipment where equipmentID = :equipmentID_from_equipment_table;
-
--- -- delete review
--- DELETE FROM Reviews where reviewID = :reviewID_from_review_table;
-
--- -- dis-associate an equipment from an order (M:N relationship)
--- DELETE FROM equipmentOrders where equipmentID = :equipmentID_from_equipment_table AND userID = UserID_from_user_table;
-
-
--- Update Total cost of user's 
--- UPDATE Orders
--- SET numItems = (select sum(amount) from OrderEquipment where orderID 
---                 IN (SELECT orderID from Orders where userID = 4))
--- WHERE orderID = (SELECT orderID from Orders where (userID = 4 AND orderDate = '2021-08-13') );
 
 
 
