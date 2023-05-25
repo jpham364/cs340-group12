3f5240f5d2e3313945f4350837e7117d4b756a1e
jpham364 <phamjonathan364@outlook.com>
2023-05-24 23:59:56 -0700

    starting on handlebars

--- app.js
+++ app.js
@@ -1,50 +1,49 @@
 /*
     SETUP
 */
+
+
+// Most of the app.js implementation is from the ecampus resource to use a node.js starter app
+// https://github.com/osu-cs340-ecampus/nodejs-starter-app
+
+
+// Step 0: set up Node.js / Express
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 7813;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 5647;                 // Set a port number at the top so it's easy to change in the future
 
 
+
+
+
+// Step 1: Connecting to SQL database
 // Database
-var db = require('./db-connector')
+var db = require('./db-connector');
 
+
+// Step 3: Integrating Handlebars
+const { engine } = require('express-handlebars');
+var expressHBS = require('express-handlebars'); // importing expressHBS
+app.engine('.hbs', engine({extname: ".hbs"})); // Create HBS engine to process template
+app.set('view engine', '.hbs') // use HBS when it sees a .hbs file
+
+
+
+// This allows access to public, static directories for now
+// Make public directory (CSS, etc,.) available to client (user's browser)
+app.use(express.static('public'));
 
 
 /*
     ROUTES
 */
-app.get('/', function(req, res)
-    {
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
-        query4 = 'SELECT * FROM diagnostic;';
+app.get('/', function(req, res){
 
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
 
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
+    res.render('index');
 
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
-
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
-
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
-
-                        // Send the results to the browser
-                        let base = "<h1>MySQL Results:</h1>"
-                        res.send(base + JSON.stringify(results));
-                    });
-                });
-            });
-        });
-    });
+});
 
 
 
